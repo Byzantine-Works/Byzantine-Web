924be1dc3adfb7751e6e604fdb4401bd1edad482
Clark Hogan <clarkhogan@me.com>
2018-11-05 09:30:40 -0800

    Updated community pill page

--- src/components/products/paymentProducts.js
+++ src/components/products/paymentProducts.js
@@ -24,8 +24,8 @@ class Home extends Component{
                         <ul>
                             <li className="clearfix">
                                 <div className="detaail">
-                                    <h3>EOS Airdrop Utility</h3>
-                                    <p>Utility tool to simplify the process of Airdrops for token launchers. Provides a CLI interface and a web-interface to wizardify a sequential set of Q&A to create token-contract, deploy, issue tokens and airdrops. This utility also estimates RAM and stake requirement based on the tokenomics for EOS vs Token ratios.</p>
+                                    <h3>EOS Contract Cost Estimator</h3>
+                                    <p>This utility estimates CPU, Memory, and RAM stakes requirements via a simple web ui.  Simply upload your ABI and WASM file and watch as the tool goes to work, regressing all the function calls within the smart contract and assesses total costs of deployment.</p>
                                 </div>
                                 <div className="prodImage">
                                     <img src="estimator.png" />
@@ -34,14 +34,38 @@ class Home extends Component{
                             
                             <li className="clearfix rightD">
                                 <div className="prodImage">
-                                    <img src={prodImage} />
+                                    <img src="api.png" />
                                 </div>
                                 <div className="detaail">
-                                    <h3>Detail One</h3>
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                                    <h3>Community EOS Blockchain as a Service API Middleware</h3>
+                                    <p>A community enabled, free to use version of our <a href="/blockchainAggregation">EOS Blockchain as a Service</a> platform</p>
                                 </div>
                             </li>
+
+                            <li className="clearfix">
+                                <div className="detaail">
+                                    <h3>EOS Airdrop Tool</h3>
+                                    <p>Built by developers for developers, this command line interface tool is a comprehensive EOS utility to simplify all EOS related on-chain functionality such as account creation, voting, delegating, undelegating, buy/sell RAM, transfer and staking.</p>
+                                    <p>It dramatically simplifies the process of Airdrops for token launches.  It provides a wizard that walks through a set of questions to create token-contract, deploy it, and then issue tokens and airdrop them to users. This utility also estimates RAM and stake requirement based on the tokenomics for EOS vs Token ratios.</p>
+
+                                </div>
+                                <div className="prodImage">
+                                    <img src="airdrop.png" />
+                                </div>
+                            </li>
+
+                           <li className="clearfix rightD">
+                                <div className="prodImage">
+                                    <img src="ideos.png" />
+                                </div>
+                                <div className="detaail">
+                                    <h3>idEOS</h3>
+                                    <p>The WORLDS FIRST Interactive Development Environment for EOS!!!  This simple to use development environment allows you to interact with the EOS testnets and mainnets or just on a local EOS node on your computer.  </p>
+                                    <p>Code, compile, deploy, transfer EOS to wallets, estimate resources required, interact with Scatter, and more with point-and-click ease.  Cool!</p>
+                                </div>
+                            </li>
+
+
                         </ul>
                     </div>
                 </div>
@@ -52,24 +76,8 @@ class Home extends Component{
                                 <li>
                                     <span><img src={prodIcon} /> </span>
                                     <div>
-                                        <h5>Detail Three</h5>
-                                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum</p>
-                                    </div>
-                                </li>
-                                
-                                <li>
-                                    <span><img src={prodIcon} /> </span>
-                                    <div>
-                                        <h5>Detail Three</h5>
-                                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum</p>
-                                    </div>
-                                </li>
-                                
-                                <li>
-                                    <span><img src={prodIcon} /> </span>
-                                    <div>
-                                        <h5>Detail Three</h5>
-                                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum</p>
+                                        <h5>Committed to the future of EOS</h5>
+                                        <p>We built tools to make our lives easier, and we want to share them with the world.  Like what you see?  Want to know more?  We'd love to hear from you!  <a href="/#contact">Drop us a line</a>, we'd love to hear from you!</p>
                                     </div>
                                 </li>
                             </ul>
